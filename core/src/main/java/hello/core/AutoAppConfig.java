dcc0f7a9617e7068a60e12637de0f19903d18290
Trilly <tttrillyyy@gmail.com>
2023-02-27 15:49:16 +0900

    6-1 Componentscan and Autowired

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // 일단은 AppConfig와 충돌하지 않기 위해 붙인 것
+)
+public class AutoAppConfig {
+
+}
