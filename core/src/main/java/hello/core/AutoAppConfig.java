eb228d22c0cae2b4a664da82f9149796fecd46b2
Trilly <tttrillyyy@gmail.com>
2023-02-27 16:53:47 +0900

    6-3 Filter

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // 일단은 AppConfig와 충돌하지 않기 위해 붙인 것
 )
