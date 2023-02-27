29691ce9476e201243cec85c38d6631793b30198
Trilly <tttrillyyy@gmail.com>
2023-02-27 15:11:15 +0900

    6.Component Scan Begin

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -44,7 +44,7 @@ public class ApplicationContextBasicFindTest {
     @DisplayName("빈 이름으로 조회 안됨")
     void findBeanByNameX() {
 
-        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
         assertThrows(NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("xxxx", MemberService.class));
     }
