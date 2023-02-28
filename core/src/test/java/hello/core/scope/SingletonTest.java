50cecaa216918baa227ec1235c5f5a1d692a690f
Trilly <tttrillyyy@gmail.com>
2023-02-28 17:41:17 +0900

    9-234 Prototype Scope and Provider

--- core/src/test/java/hello/core/scope/SingletonTest.java
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -18,7 +20,7 @@ public class SingletonTest {
         System.out.println("singletonBean1 = " + singletonBean1);
         System.out.println("singletonBean2 = " + singletonBean2);
 
-        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
         ac.close();
     }
 
